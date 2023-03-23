f80fa1f020b320cb695a514e40aea87a22e88690
Array07 <arrayashu@gmail>
2023-03-22 17:00:05 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - Checks for lowercase character
- * $c: The character to be checked
- * Return: for lowercase character or 0 for anything else
+ *_islower - Checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
 */
 
 int _islower(int c)
@@ -13,4 +13,5 @@ int _islower(int c)
 		return (1);
 	}
 	return (0);
+	_putchar('\n');
 }
